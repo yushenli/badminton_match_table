f9487aa22421dc8564700c2d4749b16cc6d6d35a
loudly <yushenli@users.noreply.github.com>
2021-08-15 11:40:55 +0900

    Change Match related data structures to support both single and double matches.

--- pkg/model/match.go
+++ pkg/model/match.go
@@ -1,11 +1,18 @@
 package model
 
-// Match is a relationship between two players.
-type Match struct {
+// Side represents one side of a match, which can be one player for singles
+// or two players for doubles.
+type Side struct {
 	Player1 *Player
 	Player2 *Player
 }
 
+// Match is a relationship between two sides.
+type Match struct {
+	Side1 Side
+	Side2 Side
+}
+
 // MatchArrangement is a slice of matches that represents an arrangement of
-// multiple simultaneous matches
+// multiple simultaneous matches.
 type MatchArrangement []Match
